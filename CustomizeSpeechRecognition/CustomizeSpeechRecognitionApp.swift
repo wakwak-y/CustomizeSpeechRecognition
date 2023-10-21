949eee5d5297217af869934429a5a811de1cd6a5
YuWakui <wak121.y@gmail.com>
2023-10-21 17:46:11 +0900

    Initial Commit

---
+++ CustomizeSpeechRecognition/CustomizeSpeechRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomizeSpeechRecognitionApp.swift
+//  CustomizeSpeechRecognition
+//
+//  Created by 和久井侑 on 2023/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomizeSpeechRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
